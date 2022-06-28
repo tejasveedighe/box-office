47cc7fb5ba87911ea872ed0d15d0bce96926e819
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-06-28 21:50:01 +0530

    input and data fetching from api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 export default function Home() {
-  return <MainPageLayout>Home</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const handleInputChange = e => {
+    setInput(e.target.value);
+  };
+
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => console.log(result));
+  };
+
+  return (
+    <MainPageLayout>
+      <input type="text" onChange={handleInputChange} value={input} />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 }
