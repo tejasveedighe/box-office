ff34429866f7bb0c133f05a926760bb0c6952101
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-06-28 20:04:45 +0530

    made title component

--- src/components/MainPageLayout.js
+++ src/components/MainPageLayout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navs from './Navs';
+import Title from './Title';
 
 export default function MainPageLayout({ children }) {
   return (
     <div>
+      <Title title="BOX OFFICE" subtitle="What are you Looking for?" />
       <Navs />
       {children}
     </div>
