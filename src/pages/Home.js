88221e34564c9dfbc743c5d325b63dac38bc04ee
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-06-29 14:07:24 +0530

    made custom render function that lists names

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import MainPageLayout from '../components/MainPageLayout';
 
 export default function Home() {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const handleInputChange = e => {
     setInput(e.target.value);
@@ -11,13 +12,27 @@ export default function Home() {
   const onSearch = () => {
     fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
       .then(r => r.json())
-      .then(result => console.log(result));
+      .then(result => setResults(result));
   };
 
   const keyDown = event => {
     if (event.keyCode === 13) {
       onSearch();
     }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) return <div>No Results</div>;
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
@@ -31,6 +46,7 @@ export default function Home() {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 }
