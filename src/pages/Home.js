ba62ace26f367b317a6c7d9d77dcdcfc0c8c2417
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-06-29 16:14:38 +0530

    created search option for show and people

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,16 @@ import getAPI from '../misc/config';
 export default function Home() {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const isShowOption = searchOption === 'shows';
 
   const handleInputChange = e => {
     setInput(e.target.value);
   };
 
   const onSearch = () => {
-    getAPI(`/search/shows?q=${input}`).then(r => setResults(r));
+    getAPI(`/search/${searchOption}?q=${input}`).then(r => setResults(r));
   };
 
   const keyDown = event => {
@@ -20,14 +23,22 @@ export default function Home() {
     }
   };
 
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+
   const renderResults = () => {
     if (results && results.length === 0) return <div>No Results</div>;
     if (results && results.length > 0) {
       return (
         <div>
-          {results.map(item => (
-            <div key={item.show.id}>{item.show.name}</div>
-          ))}
+          {results[0].show
+            ? results.map(item => (
+                <div key={item.show.id}>{item.show.name}</div>
+              ))
+            : results.map(item => (
+                <div key={item.person.id}>{item.person.name}</div>
+              ))}
         </div>
       );
     }
@@ -45,6 +56,29 @@ export default function Home() {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      <div>
+        <label htmlFor="show-search">
+          Shows
+          <input
+            id="show-search"
+            type="radio"
+            value="shows"
+            checked={isShowOption}
+            onChange={onRadioChange}
+          />
+        </label>
+
+        <label htmlFor="people-search">
+          People
+          <input
+            id="people-search"
+            type="radio"
+            value="people"
+            checked={!isShowOption}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
       {renderResults()}
     </MainPageLayout>
   );
