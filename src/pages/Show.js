cf939a481f6db7b78e9f3307a53371e21e58232e
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-07-03 16:31:04 +0530

    made changes on the api call and component mounting for not rendering when the componenet unmounts, also message dispaly for data loading and error

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -4,13 +4,37 @@ import getAPI from '../misc/config';
 
 function Show() {
   const { id } = useParams();
+
+  let isMounted = true;
+
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
-    getAPI(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results =>
-      setShow(results)
-    );
+    getAPI(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
+  if (isLoading) {
+    return <div>The Data is being Loaded</div>;
+  }
+  if (error) {
+    return <div>Error Message {error}</div>;
+  }
   console.log(show);
   return <div>Show Page</div>;
 }
