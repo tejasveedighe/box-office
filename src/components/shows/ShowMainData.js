9a709321f875a095f4811fffdab6115c88c77a7e
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-07-06 16:03:05 +0530

    created show page components and populated them with data

---
+++ src/components/shows/ShowMainData.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import IMG_PLACEHOLDER from '../../not-found.png';
+import { Star } from '../styled';
+
+export default function ShowMainData({ image, name, rating, summary, tags }) {
+  return (
+    <div>
+      <img src={image ? image.original : IMG_PLACEHOLDER} alt="show-cover" />
+      <div>
+        <div>
+          <h1>{name}</h1>
+          <div>
+            <Star />
+            <span>{rating.average || 'N/A'}</span>
+          </div>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: summary }} />
+
+        <div>
+          Tags:{' '}
+          <div>
+            {tags.map((tag, i) => (
+              <span key={i}>{tag}</span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router';
+import Cast from '../components/shows/Cast';
+import Details from '../components/shows/Details';
+import Seasons from '../components/shows/Seasons';
+import ShowMainData from '../components/shows/ShowMainData';
 import getAPI from '../misc/config';
 
 const initialState = {
@@ -56,8 +61,34 @@ function Show() {
   if (error) {
     return <div>Error Message {error}</div>;
   }
-  console.log(show);
-  return <div>Show Page</div>;
+
+  return (
+    <div>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </div>
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
+    </div>
+  );
 }
 
 export default Show;
