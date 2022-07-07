ced368c9248173fa577cf3f61501e2dfbe685016
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-07-07 11:41:18 +0530

    created custom-hook file for getting from localStorage

---
+++ src/misc/custom-hooks.js
@@ -0,0 +1,41 @@
+import { useReducer, useEffect } from 'react';
+
+// the reducer function useReducer
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'ADD': {
+      return [...prevState, action.showId];
+    }
+    case 'REMOVE': {
+      return prevState.filter(showId => showId !== action.showId);
+    }
+
+    default:
+      return prevState;
+  }
+};
+
+function usePersistedReducer(reducer, initalState, key) {
+  const [state, dispatch] = useReducer(reducer, initalState, initial => {
+    /* this is the lazy initializer that is used to set the state, it overrides the initalState argument
+     this function has more priority than the argument
+
+     this runs before the initialState we can read from the local storage then set that data as state
+
+     in this case we used this lazy initializer so that when we refresh the page the data stays
+     the localStorage will be set to null after the refresh but when we set the data to a variable it can be set to it again */
+    const persisited = localStorage.getItem(key);
+    return persisited ? JSON.stringify(persisited) : initial;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state)); // write the state to local storage
+  }, [state, key]); // when the state or key changes
+
+  return [state, dispatch];
+}
+
+// using this function so that we dont have to call the usePersistedReducer and pass a reducer to it
+export function useShows(key = 'shows') {
+  return usePersistedReducer(reducer, [], key);
+}
